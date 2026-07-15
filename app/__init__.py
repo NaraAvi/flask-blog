06caf2fd8e09bce66531e57bc885e4d5da51dbea
Naravi <170275956+NaraAvi@users.noreply.github.com>
2026-07-16 00:23:51 +0200

    add: login and registration system

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
+
+from app.models import User, Post
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
